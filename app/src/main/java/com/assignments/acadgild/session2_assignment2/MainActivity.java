066ec832fd8c007544da8215d23366025325a918
MukundPradhan <replyme.mmp@gmail.com>
2016-02-28 18:09:10 +0530

    Initial commit

---
+++ app/src/main/java/com/assignments/acadgild/session2_assignment2/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+This is a program Design the VIBGYOR
+
+ */
+
+
+
+package com.assignments.acadgild.session2_assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
